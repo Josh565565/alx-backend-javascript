9b334bd51a232e4797ebeb211ca561b2c0edc3ea
Josh565565 <smartcash565@gmail.com>
2023-06-28 02:28:18 +0100

    Fixed a bug

--- 0x01-ES6_promise/4-user-promise.js
+++ 0x01-ES6_promise/4-user-promise.js
@@ -1,6 +1,4 @@
 export default function signUpUser(firstName, lastName) {
-    return new Promise((resolve) => {
-      resolve({ firstName, lastName });
-    });
+    return Promise.resolve({ firstName, lastName });
   }
   
