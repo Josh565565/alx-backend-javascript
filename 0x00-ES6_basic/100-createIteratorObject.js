b74b339ba28aac9fb7fdd085edcd6382f094733e
Josh565565 <smartcash565@gmail.com>
2023-07-01 00:24:55 +0100

    Fixed a bug in task 100

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,19 +1,23 @@
 export default function createIteratorObject(report) {
-  const all = Object.values(report.allEmployees).reduce((a, b) => {
-    a.push(...b);
-    return a;
-  }, []);
-  let currIndex = 0;
-  const maxIndex = all.length;
-  return {
-    next() {
-      if (currIndex < maxIndex) {
-        const result = { value: all[currIndex], done: false };
-        currIndex += 1;
-        return result;
+  let iterable = [];
+
+  if (!report.allEmployees || typeof report.allEmployees !== 'object') {
+    return iterable;
+  }
+
+  //   for (const r of Object.values(report.allEmployees)) {
+  //     iterable.push(...r);
+  //   }
+
+  iterable = {
+    * [Symbol.iterator]() {
+      for (const value of Object.values(report.allEmployees)) {
+        for (const i of value) {
+          yield i;
+        }
       }
-      return { value: null, done: true };
     },
-    [Symbol.iterator]: () => this.next(),
   };
+
+  return iterable;
 }
